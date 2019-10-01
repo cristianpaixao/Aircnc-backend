f69b947cc5877112955cedc5f52da066e8bae4ce
Cristian Paixao <cristian.paixao@outlook.com>
2019-10-01 18:11:58 -0300

    Adding mongoose to mongodb connection

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const routes = require('./routes')
+const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
 
-app.use(express.json())
-app.use(routes)
+mongoose.connect('mongodb://localhost:27017/aircnc', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
